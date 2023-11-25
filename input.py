45adc528171817640441a68f78c4b8b4d83a4863
ziani abderrahim <zianiabderrahim@gmail.com>
2023-11-25 23:59:09 +0100

    input

--- input.py
+++ input.py
@@ -11,3 +11,42 @@ mName = mName.strip().capitalize()
 lName = lName.strip().capitalize()
 
 print(f"Hello {fName} {mName:.1s} {lName} Happy To See You.")
+
+# ---------------------------
+# -- Practical Slice Email --
+# ---------------------------
+
+theName = input('What\'s Your Name ?').strip().capitalize()
+theEmail = input('What\'s Your Email ?').strip()
+
+theUsername = theEmail[:theEmail.index("@")]
+theWebsite = theEmail[theEmail.index("@") + 1:]
+
+print(f"Hello {theName} Your Email Is {theEmail}")
+print(f"Your Username Is {theUsername} \nYour Website Is {theWebsite}")
+
+email = "Osama@elzero.org"
+# print(email[:email.index("@")])
+
+# -------------------------------------
+# -- Practical Your Age Full Details --
+# -------------------------------------
+
+# Input Age
+age = int(input('What\'s Your Age ? ').strip())
+
+# Get Age in All Time Units
+months = age * 12
+weeks = months * 4
+days = age * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+
+print('You Lived For:')
+print(f"{months} Months.")
+print(f"{weeks:,} Weeks.")
+print(f"{days:,} Days.")
+print(f"{hours:,} Hours.")
+print(f"{minutes:,} Minutes.")
+print(f"{seconds:,} Seconds.")
