8c1e5160bb6997e42ab619637eb69d7012ee4033
alziani <99541666+alziani@users.noreply.github.com>
2024-01-03 00:50:34 +0100

    Update func.py

--- func.py
+++ func.py
@@ -12,7 +12,7 @@
 # [9] A Function Is For All Team and All Apps
 # ----------------------------------------
 
-#ahlan si brahim
+#ahlan si brahim mn boukhalef 
 def function_name():
 
   return "Hello Python From Inside Function"
